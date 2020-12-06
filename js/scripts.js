5fa41987d355ab0e1c0e34ddaa49ad1f2ae4139d
Zackary <McMurtry>
2020-12-06 03:46:29 -0500

    Changed pages to be more mobile friendly

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,31 @@ function initMap() {
         map: map,
     });
 }
+
+function hamburgerControl(event) {
+    event.stopPropagation();
+    if (document.getElementById("hamburger-toggle").hasAttribute("checked")) {
+        document.getElementById("hamburger-toggle").removeAttribute("checked");
+        document.getElementById("glazed").style.display = "none";
+        document.body.style.overflow = "";
+        document.getElementsByClassName("sticky")[0].style.left = "-80%";
+    } else {
+        document.getElementById("hamburger-toggle").setAttribute("checked", "true");
+        document.getElementById("glazed").style.display = "block";
+        document.body.style.overflow = "hidden";
+        document.getElementsByClassName("sticky")[0].style.left = "0";
+    }
+}
+
+function preventCollapse(event) {
+    event.stopPropagation();
+}
+
+function collapse() {
+    if (document.getElementById("hamburger-toggle").hasAttribute("checked")) {
+        document.getElementById("hamburger-toggle").removeAttribute("checked");
+        document.getElementById("glazed").style.display = "none";
+        document.body.style.overflow = "";
+        document.getElementsByClassName("sticky")[0].style.left = "-80%";
+    }
+}
